6157a14ea3770e7e86a59414d7d47ffec6327447
SwetanshuSingh <swetanshusingh17@gmail.com>
2025-06-24 12:05:53 +0530

    feat: google sign & middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+
 const Home = () => {
+  const signIn = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "/profile",
+    });
+  };
+
   return (
-    <main className="relative w-full h-screen flex justify-center items-center bg-[#F1F1F1]">
-      <h2 className="text-2xl font-medium">Hello World</h2>
+    <main className="relative w-full h-screen flex flex-col justify-center items-center gap-3 bg-[#F1F1F1]">
+      <h2 className="text-3xl font-medium">welcome to pluto</h2>
+      <button
+        onClick={signIn}
+        className="border border-gray-300 px-5 py-2 rounded-3xl hover:bg-gray-300 transition-colors duration-150"
+      >
+        continue to app
+      </button>
     </main>
   );
 };
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,7 +7,10 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: {
-      schema,
+      user: schema.user,
+      account: schema.account,
+      session: schema.session,
+      verification: schema.verification
     },
   }),
   socialProviders: {
