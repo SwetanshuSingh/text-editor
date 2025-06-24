1b2f08b32f6c11f73df10bbe5b6b1ab8823096db
SwetanshuSingh <swetanshusingh17@gmail.com>
2025-06-24 11:03:12 +0530

    feat: init better-auth with drizzle orm

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="w-full max-h-screen h-screen flex justify-start bg-[#F5F5F5]">
+    <div className="w-full max-h-screen h-screen flex justify-start bg-[#F5F5F5] overflow-hidden">
       <Sidebar />
       <div className="w-full p-3 pl-0">{children}</div>
     </div>
