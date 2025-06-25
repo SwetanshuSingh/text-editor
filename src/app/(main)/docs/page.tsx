dde41256f2944817a668ce4f1cc799dcbe7d9619
SwetanshuSingh <swetanshusingh17@gmail.com>
2025-06-25 12:53:53 +0530

    feat: fetch user docs and display

--- src/app/(main)/docs/page.tsx
+++ src/app/(main)/docs/page.tsx
@@ -1,3 +1,5 @@
+import { getDocuments } from "@/actions/get-documents";
+import DocIcon from "@/components/ui/doc-icon";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -8,7 +10,15 @@ const DocsPage = async () => {
     redirect(`${process.env.BETTER_AUTH_URL as string}/editor/${uuid}`);
   };
 
-  
+  const response = await getDocuments();
+  const documents = response.data;
+
+  console.log("Docs", response);
+
+  if (response.status == "error") {
+    // TODO: handle error state here
+  }
+
   return (
     <main className="w-full h-full bg-[#F8F8F8] p-5 flex flex-col gap-5 rounded-md outline outline-[#E4E4E4]">
       <div className="w-full flex justify-end items-center">
@@ -22,18 +32,27 @@ const DocsPage = async () => {
         </form>
       </div>
 
-      <div className="flex justify-start items-center gap-5">
-        <Link href="/editor/124">
-          <div className="w-40 h-52 bg-[#F2F2F2] flex flex-col gap-3 font-shadows p-4 rounded-xl hover:bg-white cursor-pointer transition-all duration-150 hover:translate-y-[-2px]">
-            <p className="text-[#5D5D5D] font-semibold">Machine Learning</p>
-            <p className="text-sm text-[#868686]">
-              {" "}
-              Software is deterministic. Given some input, if you run the
-              program again, you will get the same output.
-            </p>
-          </div>
-        </Link>
-      </div>
+      {!documents || documents.length === 0 ? (
+        <div className="h-full flex flex-col justify-center items-center gap-2">
+          <DocIcon />
+          <p className="text-[#8C8C8C] font-semibold tracking-tight">
+            no documents yet.
+          </p>
+        </div>
+      ) : (
+        <div className="flex justify-start items-center gap-5">
+          <Link href="">
+            <div className="w-40 h-52 bg-[#F2F2F2] flex flex-col gap-3 font-shadows p-4 rounded-xl hover:bg-white cursor-pointer transition-all duration-150 hover:translate-y-[-2px]">
+              <p className="text-[#5D5D5D] font-semibold">Machine Learning</p>
+              <p className="text-sm text-[#868686]">
+                {" "}
+                Software is deterministic. Given some input, if you run the
+                program again, you will get the same output.
+              </p>
+            </div>
+          </Link>
+        </div>
+      )}
     </main>
   );
 };
