f15aeafbcc1f354dd63a9599753e0b44d85b9a77
SwetanshuSingh <swetanshusingh17@gmail.com>
2025-07-25 11:12:03 +0530

    update: add sign out btn

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,17 +2,37 @@
 
 import FileIcon from "@/components/ui/file-icon";
 import UserIcon from "@/components/ui/user-icon";
+import LogoutIcon from "@/components/ui/log-out-icon";
 import Link from "next/link";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 const Sidebar = () => {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+      },
+    });
+  };
+
   return (
-    <aside className="w-fit h-full flex flex-col gap-6 items-center p-3 pt-16">
-      <Link href="/profile">
-        <UserIcon />
-      </Link>
-      <Link href="/docs">
-        <FileIcon />
-      </Link>
+    <aside className="w-fit h-full flex flex-col items-center justify-between px-3 pt-16 pb-3.5">
+      <div className="flex flex-col items-center gap-6">
+        <Link href="/profile">
+          <UserIcon />
+        </Link>
+        <Link href="/docs">
+          <FileIcon />
+        </Link>
+      </div>
+      <button onClick={handleSignOut}>
+        <LogoutIcon />
+      </button>
     </aside>
   );
 };
---
+++ src/components/ui/log-out-icon.tsx
@@ -0,0 +1,22 @@
+const LogoutIcon = () => {
+  return (
+    <svg
+      className="text-[#8C8C8C] hover:text-black/80 cursor-pointer transition-colors duration-200"
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4m7 14l5-5l-5-5m5 5H9"
+      />
+    </svg>
+  );
+};
+
+export default LogoutIcon;
